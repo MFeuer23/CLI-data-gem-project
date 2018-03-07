e764545fb88699dca5279f87a3704645587bf062
Mallory Feuer <mallory.feuer@gmail.com>
2018-03-07 20:05:07 +0000

    refactored my cli with a case statment, made mta_service_status.rb my environment

--- mta_service_status/lib/mta_service_status.rb
+++ mta_service_status/lib/mta_service_status.rb
@@ -1,6 +1,2 @@
 require_relative "./mta_service_status/version"
 require_relative "./mta_service_status/cli"
-
-module MtaServiceStatus
-  # Your code goes here...
-end
--- mta_service_status/lib/mta_service_status/cli.rb
+++ mta_service_status/lib/mta_service_status/cli.rb
@@ -1,59 +1,58 @@
 class MtaServiceStatus::Cli
 
   def call
-    puts "Welcome!"
-    puts "To view the current status of MTA service, enter status"
-    puts "To view the status of one train, enter train line"
-    input = gets.chomp.downcase
-    if input == "status"
-      puts "current train status!"
-      #scrape mta service advisory website
-      #print status of all trains
-    elsif input == "1" || input == "2" || input == "3"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "4" || input == "5" || input == "6"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "7"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "a" || input == "c" || input == "e"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "b" || input == "d" || input == "f" || input == "m"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "g"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "j" || input == "z"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "l"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "n" || input == "q" || input == "r" || input == "w"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "s"
-      puts "good! #{input}"
-      #Good to go, or more specific delay detail
-    elsif input == "exit"
-      nil
-    else
-      puts "please enter train line, status, or exit"
-    end
+    menu
+    goodbye
   end
 
-    #123
-    #456
-    #7
-    #ACE
-    #BDFM
-    #G
-    #JZ
-    #L
-    #NQRW
-    #S
+  def menu
+
+      puts "To view the current status of MTA service, enter status"
+      puts "To view the status of one train, enter train line"
+      input = gets.chomp
+      case input
+      when"status"
+          puts "current train status!"
+          #scrape mta service advisory website
+          #print status of all trains
+      when "1", "2", "3"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      when "4", "5", "6"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      when "7"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      when "a", "c", "e"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      when "b", "d", "f", "m"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      when "g"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      when "j", "z"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      when "l"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      when "n", "q", "r", "w"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      when "s"
+          puts "good! #{input}"
+          #Good to go, or more specific delay detail
+      else
+          puts "please enter train line, status, or exit"
+      end
+  
+  end
+
+  def goodbye
+    "Happy Travels! Know before you go :)"
+  end
+
 end
